3dff3b3ed6b77c9737b79995d1acc820111f5653
Felipe Cecatto <felicecatto@hotmail.com>
2020-08-10 19:30:51 -0300

    Adicionado o jquery-mask-plugin e usando o método mask para o campo input

--- src/Selector.js
+++ src/Selector.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import './Selector.css';
+import $ from 'jquery';
+import 'jquery-mask-plugin/dist/jquery.mask.min';
+
+//Completa automaticamente a barra entre a data
+$(document).ready(function(){
+    $('#inputFrom').mask('00/00/0000');
+});
+$(document).ready(function(){
+    $('#inputTo').mask('00/00/0000');
+})
+
 
 export function Selector()
 {   
@@ -10,12 +21,12 @@ export function Selector()
             </div>
             <form className="Data" id="divData">
                 <input type="text" className="From" id="inputFrom" maxLength="10" size="10" placeholder="dd/mm/aaaa"
-                pattern="[1-9]{2}+/[1-9]{2}+/[0-9]{4}" required></input>
+                pattern="[1-9]{2}/[1-9]{2}/[0-9]{4}" required></input>
 
                 <p> até </p>
 
                 <input type="text" className="To" id="inputTo" maxLength="10" size="10" placeholder="dd/mm/aaaa"
-                pattern="[1-9]{2}+/[1-9]{2}+/[0-9]{4}" required></input>
+                pattern="[1-9]{2}/[1-9]{2}/[0-9]{4}" required></input>
             </form>
         </div>
     );
